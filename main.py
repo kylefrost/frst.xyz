d4412646c867ca1d7a82314d55b342cdb9acff53
Kyle Frost <kylefrost@comcast.net>
2015-08-15 15:17:51 -0400

    Add main.py and HTML templates

---
+++ main.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect
+import MySQLdb, string, random, dbc, time
+
+app = Flask(__name__)
+
+# Load Index page
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+# Generate alias and of size "size"
+def gen_rand_alias(size, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+# Insert new url and alias into database
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    # Get randomly generated alias
+    alias = gen_rand_alias(10)
+    
+    # Get URL and password from POST Request
+    URL = request.form.get('url')
+    password = request.form.get('password')
+    
+    # If password is incorrect, Rick Roll
+    if password not in dbc.passwords or password is None:
+        return render_template("sorry.html", message="Sorry, only Kyle can use this URL shortener.")
+
+    # Create database connection
+    db = MySQLdb.connect(host=dbc.server, user=dbc.user, passwd=dbc.passwd, db=dbc.db)
+    cursor = db.cursor()
+    
+    # Insert Redirect URL and Alias into database
+    cursor.execute("INSERT INTO " + dbc.urltbl + " (url, alias) VALUES (\'" + URL + "\', \'" + alias + "\');")
+    
+    # Commit to database and close connections
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return render_template("link.html", url="http://frst.xyz/" + alias)
+
+@app.route('/<alias>', methods=['GET', 'POST'])
+def alias(alias):
+    # Create database connection
+    db = MySQLdb.connect(host=dbc.server, user=dbc.user, passwd=dbc.passwd, db=dbc.db)
+    cursor = db.cursor()
+    
+    # Insert Redirect URL and Alias into database
+    cursor.execute("SELECT url FROM " + dbc.urltbl + " WHERE alias = \"" + alias + "\"")
+
+    result = cursor.fetchone()
+    
+    if result is None:
+        return render_template("sorry.html", message="Sorry, that URL alias wasn't found.")
+    
+    url = result[0]
+
+    cursor.execute("INSERT INTO " + dbc.cltbl + " (ip, alias, dateClicked, userAgent, browser, platform) VALUES (\"" + str(request.remote_addr) + "\", \"" + alias + "\", \"" + time.strftime('%Y-%m-%d %H:%M:%S') + "\", \"" + str(request.headers.get('User-Agent')) + "\", \"" + str(request.user_agent.browser) +  "\", \"" + str(request.user_agent.platform) + "\")")
+
+    # Close connections
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return redirect(str(result[0]), code=302)
+
+# Run Flask app on load
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
